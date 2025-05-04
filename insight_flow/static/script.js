1e1a72b4f8dba8eea0cb2a1753c4950844a82d47
rloss <rlossqianss@gmail.com>
2025-05-04 19:29:57 +0900

    토글버튼

--- insight_flow/static/script.js
+++ insight_flow/static/script.js
@@ -1,1 +1,10 @@
 // script.js - 사이드바 토글 등 JS
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggle-btn");
+    const sidebar = document.getElementById("sidebar");
+  
+    toggleBtn.addEventListener("click", function () {
+      sidebar.classList.toggle("closed");
+    });
+  });
+  
