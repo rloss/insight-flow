2458ffa1c92b1193c9c43b0bb1a555060dcc7435
rloss <rlossqianss@gmail.com>
2025-05-04 15:47:21 +0900

    룰루

--- insight_flow/app.py
+++ insight_flow/app.py
@@ -20,5 +20,7 @@ app.register_blueprint(community_bp)
 app.static_folder = "static"
 app.template_folder = "templates"
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=True)
